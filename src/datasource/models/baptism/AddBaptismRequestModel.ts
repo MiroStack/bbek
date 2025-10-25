d07a3987498f62e69236c8c21070777dbf0a069b
Miro Sarte <mirosarte.work@gmail.com>
2025-10-25 12:42:33 +0800

    Updated code done creating baptism schedule module

---
+++ src/datasource/models/baptism/AddBaptismRequestModel.ts
@@ -0,0 +1,16 @@
+export interface AddBaptismRequestModel{
+    firstname:string;
+    middlename:string;
+    lastname:string;
+    birthdate:string;
+    age:number;
+    sex:string;
+    address:string;
+    email:string;
+    contactNo:string;
+    baptismDate:string;
+    baptismOfficiant:string;
+    baptismOfficiantId:number;
+    location:string;
+    baptismStringDate:string
+}
